2b820cca69588121b831413f1545d5bd04cac843
João Lucas Sidney Rodrigues <43157844+JaoLucass@users.noreply.github.com>
2024-05-14 18:47:29 +0000

    CRUD concluido com sucesso

--- index.js
+++ index.js
@@ -14,17 +14,63 @@ const itens = ['carne', 'ovo', 'batata']
 
 // Endpoint de Read All [GET] /item
 app.get('/item', function (req, res) {
-    res.send('Read All Funcionando!')
+    res.send(itens.filter[Boolean])
 })
 
 // Endpoint de Read By ID [GET]/item/:id
-app.get('/item/:id', function(req, res){
+app.get('/item/:id', function(req, res) {
     const id = req.params.id
 
     // acessado o item da lista usando ID - 1
     const item = itens[id-1]
 
     res.send(item)
+})
+
+// Sinalizando que todo o corpo de requisição
+// virá como JSON
+app.use(express.json())
+
+// Endpoint de Create [POST] /item
+app.post('/item', function(req,res) {
+    // Acessando o corpo da requisição
+    const body = req.body
+
+    // Acessar o item no corpo da requisição
+    const novoItem = body.nome
+
+    // Adicionar novo item na lista
+    itens.push(novoItem)
+
+    res.send('Item adicionado com sucesso: ' + novoItem)
+})
+
+// Endpoint de Update [PUT] /item/:id
+app.put('/item/:id', function (req, res) {
+    // Acessar o ID do parametro de rota
+    const id = req.params.id
+
+    // Acessar o item a ser atualizado, a partir do
+    // corpo da requisição
+    const body = req.body
+    const atualizarItem = body.nome
+
+    // Atualizar na lista o item recebido
+    itens[id-1] = atualizarItem
+
+    res.send('Item atualizado com sucesso: ' + id + ', ' + atualizarItem)
+})
+
+// Endpoint de Delete [DELETE] /item/:id
+app.delete('/item/:id', function (req, res) {
+    // Acessar o parametro de rota ID
+    const id = req.params.id
+
+    // Executa a operação de exclusão desse item pelo índice
+    delete itens[id - 1]
+
+    //Enviamos uma mensagem de sucesso
+    res.send('Item removido com sucesso: ' + id)
 })
 
 app.listen(3000)
