887e3476ac49398dcbfbcaeb882ae2d9a1ee0834
João Lucas Sidney Rodrigues <43157844+JaoLucass@users.noreply.github.com>
2024-05-14 17:31:57 +0000

    instalação do nodemon e implementação de Read All e Read By ID

--- index.js
+++ index.js
@@ -8,4 +8,23 @@ app.get('/', function (req, res) {
 app.get('/oi', function (req, res) {
     res.send('Olá mundo!')
 })
+
+// Lista de Itens
+const itens = ['carne', 'ovo', 'batata']
+
+// Endpoint de Read All [GET] /item
+app.get('/item', function (req, res) {
+    res.send('Read All Funcionando!')
+})
+
+// Endpoint de Read By ID [GET]/item/:id
+app.get('/item/:id', function(req, res){
+    const id = req.params.id
+
+    // acessado o item da lista usando ID - 1
+    const item = itens[id-1]
+
+    res.send(item)
+})
+
 app.listen(3000)
